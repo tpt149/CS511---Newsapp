d011c0054faf3a313ea77b7c196c8c3102897313
Thao Tran <88588832+tpt149@users.noreply.github.com>
2023-01-03 14:47:21 +0700

    menu

--- Newsapp/Newsapp/Form1.cs
+++ Newsapp/Newsapp/Form1.cs
@@ -12,9 +12,66 @@ namespace Newsapp
 {
     public partial class Form1 : Form
     {
+        bool sidebarExpand;
+        bool homeCollapsed;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void sidebarTime_Tick(object sender, EventArgs e)
+        {
+            if (sidebarExpand)
+            {
+                sidebar.Width -= 10;
+                if(sidebar.Width==sidebar.MinimumSize.Width)
+                {
+                    sidebarExpand = false;
+                    sidebarTime.Stop();
+                }
+            }
+            else
+            {
+                sidebar.Width += 10;
+                if (sidebar.Width == sidebar.MaximumSize.Width)
+                {
+                    sidebarExpand = true;
+                    sidebarTime.Stop();
+                }
+            }
+
+        }
+
+        private void menuButton_Click(object sender, EventArgs e)
+        {
+            sidebarTime.Start();
+        }
+
+        private void homeTime_Tick(object sender, EventArgs e)
+        {
+            if (homeCollapsed)
+            {
+                homeContainer.Height += 10;
+                if (homeContainer.Height == homeContainer.MaximumSize.Height)
+                {
+                    homeCollapsed = false;
+                    homeTime.Stop();
+                }
+            }
+            else
+            {
+                homeContainer.Height -= 10;
+                if (homeContainer.Height == homeContainer.MinimumSize.Height)
+                {
+                    homeCollapsed = true;
+                    homeTime.Stop();
+                }
+            }
+        }
+
+        private void btn_home_Click(object sender, EventArgs e)
+        {
+            homeTime.Start();
         }
     }
 }
